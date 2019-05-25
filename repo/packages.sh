0cd039f4dfb13d7e16d3149fbb35c51511ae0794
Flipp3rrr <philipkreuning@icloud.com>
2019-05-25 17:58:56 +0200

    Removed all .DS_Store files and autoremove them in the future

--- repo/packages.sh
+++ repo/packages.sh
@@ -2,3 +2,11 @@ dpkg-deb -b SimpleClockLS
 dpkg-deb -b StopAnemone
 dpkg-deb -b StopSnowboard
 dpkg-deb -b StopWinterboard
+cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/SimpleClockLS'
+find . -name '.DS_Store' -type f -delete
+cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopAnemone'
+find . -name '.DS_Store' -type f -delete
+cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopSnowboard'
+find . -name '.DS_Store' -type f -delete
+cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopWinterboard'
+find . -name '.DS_Store' -type f -delete
