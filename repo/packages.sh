8f86e30c25ea225d71b84ed61ea0843b415588ee
Flipp3rrr <philipkreuning@icloud.com>
2019-05-25 20:43:04 +0200

    Fix the removal of .DS_Store

--- repo/packages.sh
+++ repo/packages.sh
@@ -1,7 +1,3 @@
-dpkg-deb -b SimpleClockLS
-dpkg-deb -b StopAnemone
-dpkg-deb -b StopSnowboard
-dpkg-deb -b StopWinterboard
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/SimpleClockLS'
 find . -name '.DS_Store' -type f -delete
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopAnemone'
@@ -10,3 +6,8 @@ cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopSnowboard'
 find . -name '.DS_Store' -type f -delete
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopWinterboard'
 find . -name '.DS_Store' -type f -delete
+
+dpkg-deb -b SimpleClockLS
+dpkg-deb -b StopAnemone
+dpkg-deb -b StopSnowboard
+dpkg-deb -b StopWinterboard
