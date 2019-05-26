eba6d8a6bb4679d712f06d4864f574be7c19382c
Flipp3rrr <philipkreuning@icloud.com>
2019-05-26 10:19:41 +0200

    FIx a few things

--- repo/packages.sh
+++ repo/packages.sh
@@ -1,5 +1,3 @@
-cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/SimpleClockLS'
-find . -name '.DS_Store' -type f -delete
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopAnemone'
 find . -name '.DS_Store' -type f -delete
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopSnowboard'
@@ -7,7 +5,7 @@ find . -name '.DS_Store' -type f -delete
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopWinterboard'
 find . -name '.DS_Store' -type f -delete
 
-dpkg-deb -b SimpleClockLS
+cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo'
 dpkg-deb -b StopAnemone
 dpkg-deb -b StopSnowboard
 dpkg-deb -b StopWinterboard
