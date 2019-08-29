bcd2b6a78ea86d6664ebd1970219f775ab7fbe5e
Flipp3rrr <philipkreuning@icloud.com>
2019-08-29 21:21:33 +0200

    Fix build script

--- repo/packages.sh
+++ repo/packages.sh
@@ -5,6 +5,7 @@ find . -name '.DS_Store' -type f -delete
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopWinterboard'
 find . -name '.DS_Store' -type f -delete
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/ClassicAppleBoot'
+find . -name '.DS_Store' -type f -delete
 
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo'
 dpkg-deb -b StopAnemone
