ee0168072d06c71097b21df395113905ef8ddde6
Flipp3rrr <philipkreuning@icloud.com>
2019-09-10 20:15:36 +0200

    New build scripts

--- repo/UpdatePackages.sh
+++ repo/UpdatePackages.sh
@@ -1,14 +1,30 @@
+echo "Updating repository, this may take up to 15 seconds."
+
+echo "Removing old packages file..."
+rm -r Packages.bz2
+
+echo "Removing .DS_Store files..."
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopAnemone'
 find . -name '.DS_Store' -type f -delete
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopSnowboard'
 find . -name '.DS_Store' -type f -delete
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/StopWinterboard'
 find . -name '.DS_Store' -type f -delete
-cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo/ClassicAppleBoot'
-find . -name '.DS_Store' -type f -delete
 
+sleep 3
+
+echo "Packaging dummy packages..."
 cd '/Users/gebruiker/Documents/GitHub/ud-team.github.io/repo'
 dpkg-deb -b StopAnemone
 dpkg-deb -b StopSnowboard
 dpkg-deb -b StopWinterboard
-dpkg-deb -b ClassicAppleBoot
+
+sleep 3
+
+echo "Adding packages to repository..."
+dpkg-scanpackages -m . /dev/null >Packages
+bzip2 Packages
+
+sleep 3
+
+echo "Finished updating packages!"
